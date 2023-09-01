cd486343b9fd7302bae55ced91e13c0c409bdea1
Alexey Baikov <baikov52@gmail.com>
2023-09-01 14:41:13 +0300

    Hotfix: fix media on local

--- config/settings/base.py
+++ config/settings/base.py
@@ -294,8 +294,8 @@ CELERY_TASK_SEND_SENT_EVENT = True
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.BasicAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
