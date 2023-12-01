8bfd2ed257e8db424f8d0e5d8c44e2a739d8e5d2
Alexey Baikov <baikov52@gmail.com>
2023-12-01 14:29:44 +0300

    fix parser oseni

--- backend/orders/services.py
+++ backend/orders/services.py
@@ -246,7 +246,7 @@ class OseniParserV2(Parser):
 
             customer_product, _ = CustomerProduct.objects.get_or_create(
                 name=row[self._PRODUCT_COLUMN_NAME].strip(),
-                vendor_code=str(row[self._VENDOR_CODE_COLUMN_NAME]).strip(),
+                vendor_code=str(int(row[self._VENDOR_CODE_COLUMN_NAME])).strip(),
                 customer=self.customer,
             )
             if customer_product not in unique_customer_products:
